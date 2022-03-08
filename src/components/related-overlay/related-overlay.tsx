fadc588a99c8a78ded902c6e6cd36f4a3d5463c8
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2022-03-08 16:49:58 +0200

    responsivity - player size XL

--- src/components/related-overlay/related-overlay.tsx
+++ src/components/related-overlay/related-overlay.tsx
@@ -7,6 +7,7 @@ import {PageAction} from './page-action';
 import * as styles from './related-overlay.scss';
 
 const {connect} = KalturaPlayer.ui.redux;
+const {PLAYER_SIZE} = KalturaPlayer.ui.components;
 
 const FIRST_PAGE_ENTRIES_NUM = 7;
 const PAGE_ENTRIES_NUM = 8;
@@ -22,82 +23,118 @@ const pageReducer = (state: {prevPage: number; currPage: number; nextPage: numbe
 };
 
 const mapStateToProps = (state: any) => {
+  const {engine, shell} = state;
+  const {isPaused, isPlaybackEnded} = engine;
   return {
-    isPaused: state.engine.isPaused,
-    isPlaybackEnded: state.engine.isPlaybackEnded
+    isPaused,
+    isPlaybackEnded,
+    sizeBreakpoint: shell.playerSize
   };
 };
 
-const RelatedOverlay = connect(mapStateToProps)(
-  ({relatedManager, isPaused, isPlaybackEnded}: {relatedManager: RelatedManager; isPaused: boolean; isPlaybackEnded: boolean}) => {
-    const [isVisible, setIsVisible] = useState(false);
-    const [countdown, setCountdown] = useState(-1);
-    const [pageAnimation, setPageAnimation] = useState('');
-    const [pageAction, setPageAction] = useState(PageAction.NOTHING);
+interface RelatedOverlayProps {
+  relatedManager: RelatedManager;
+  isPaused: boolean;
+  isPlaybackEnded: boolean;
+  sizeBreakpoint: string;
+}
 
-    const [pageState, dispatch] = useReducer(pageReducer, {
-      prevPage: -1,
-      currPage: 0,
-      nextPage: 1
-    });
+const RelatedOverlay = connect(mapStateToProps)(({relatedManager, isPaused, isPlaybackEnded, sizeBreakpoint}: RelatedOverlayProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [countdown, setCountdown] = useState(-1);
+  const [pageAnimation, setPageAnimation] = useState('');
+  const [pageAction, setPageAction] = useState(PageAction.NOTHING);
 
-    useEffect(() => {
-      if (pageAction !== PageAction.NOTHING) {
-        setPageAnimation(pageAction === PageAction.NEXT ? styles.slideLeft : styles.slideRight);
-      }
-    }, [pageAction]);
+  const [pageState, dispatch] = useReducer(pageReducer, {
+    prevPage: -1,
+    currPage: 0,
+    nextPage: 1
+  });
 
-    const onAnimationEnd = () => {
-      if (pageAction !== PageAction.NOTHING) {
-        dispatch(pageAction);
-        setPageAnimation('');
-        setPageAction(PageAction.NOTHING);
-      }
-    };
-
-    if (!relatedManager.entries.length) {
-      setIsVisible(false);
-    } else if (!isPlaybackEnded) {
-      setIsVisible(isPaused && relatedManager.showOnPlaybackPaused);
-      setCountdown(-1);
-    } else if (relatedManager.showOnPlaybackDone) {
-      setIsVisible(true);
-      setCountdown(relatedManager.countdownTime);
-    } else {
-      setIsVisible(false);
+  useEffect(() => {
+    if (pageAction !== PageAction.NOTHING) {
+      setPageAnimation(pageAction === PageAction.NEXT ? styles.slideLeft : styles.slideRight);
     }
+  }, [pageAction]);
 
-    const {entries} = relatedManager;
+  const onAnimationEnd = () => {
+    if (pageAction !== PageAction.NOTHING) {
+      dispatch(pageAction);
+      setPageAnimation('');
+      setPageAction(PageAction.NOTHING);
+    }
+  };
 
-    const showPagination = relatedManager.entries.length > FIRST_PAGE_ENTRIES_NUM;
-    const {prevPage, currPage, nextPage} = pageState;
+  if (!relatedManager.entries.length) {
+    setIsVisible(false);
+  } else if (!isPlaybackEnded) {
+    setIsVisible(isPaused && relatedManager.showOnPlaybackPaused);
+    setCountdown(-1);
+  } else if (relatedManager.showOnPlaybackDone) {
+    setIsVisible(true);
+    setCountdown(relatedManager.countdownTime);
+  } else {
+    setIsVisible(false);
+  }
 
-    return (
+  const {entries} = relatedManager;
+
+  const showPagination = relatedManager.entries.length > FIRST_PAGE_ENTRIES_NUM;
+  const {prevPage, currPage, nextPage} = pageState;
+
+  switch (sizeBreakpoint) {
+    case PLAYER_SIZE.TINY: {
+      break;
+    }
+    case PLAYER_SIZE.EXTRA_SMALL: {
+      break;
+    }
+    case PLAYER_SIZE.SMALL: {
+      break;
+    }
+    case PLAYER_SIZE.MEDIUM: {
+      break;
+    }
+    case PLAYER_SIZE.LARGE: {
+      break;
+    }
+    case PLAYER_SIZE.EXTRA_LARGE: {
+      break;
+    }
+    default:
+      break;
+  }
+
+  return (
+    <div>
       <div className={`${styles.relatedOverlay} ${isVisible ? '' : styles.hidden}`}>
         <RelatedContext.Provider value={{relatedManager}}>
           <div className={styles.content}>
-            {showPagination ? (
+            {!showPagination ? (
+              <div className={`${styles.gridPagesWrapper} ${pageAnimation}`} onAnimationEnd={onAnimationEnd}>
+                <GridPages data={relatedManager.entries} countdown={countdown} prevPage={prevPage} currPage={currPage} nextPage={nextPage} />
+              </div>
+            ) : (
               <>
-                <div className={styles.arrowRight}>
-                  <ArrowRight onClick={() => setPageAction(PageAction.PREV)} disabled={currPage === 0} />
+                <div className={`${styles.arrow} ${styles.arrowLeft}`}>
+                  <ArrowLeft onClick={() => setPageAction(PageAction.PREV)} disabled={currPage === 0} />
                 </div>
-                <div className={styles.arrowLeft}>
-                  <ArrowLeft
+                <div className={`${styles.gridPagesWrapper} ${pageAnimation}`} onAnimationEnd={onAnimationEnd}>
+                  <GridPages data={relatedManager.entries} countdown={countdown} prevPage={prevPage} currPage={currPage} nextPage={nextPage} />
+                </div>
+                <div className={`${styles.arrow} ${styles.arrowRight}`}>
+                  <ArrowRight
                     onClick={() => setPageAction(PageAction.NEXT)}
                     disabled={currPage > 0 && entries.length <= nextPage * PAGE_ENTRIES_NUM}
                   />
                 </div>
               </>
-            ) : undefined}
-
-            <div className={`${styles.gridPagesWrapper} ${pageAnimation}`} onAnimationEnd={onAnimationEnd}>
-              <GridPages data={relatedManager.entries} countdown={countdown} prevPage={prevPage} currPage={currPage} nextPage={nextPage} />
-            </div>
+            )}
           </div>
         </RelatedContext.Provider>
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
 
 export {RelatedOverlay};
