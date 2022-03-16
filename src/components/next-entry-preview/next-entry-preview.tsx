2a5bb72c87c7b8281939cdddb64e98ce523260d5
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2022-03-16 18:52:42 +0200

    cancel button text style

--- src/components/next-entry-preview/next-entry-preview.tsx
+++ src/components/next-entry-preview/next-entry-preview.tsx
@@ -1,11 +1,17 @@
+import {useContext} from 'preact/hooks';
+
 import {EntryImage} from 'components/entry-image/entry-image';
 import {Sources} from 'types/sources';
 
 const {Icon, IconType} = KalturaPlayer.ui.components;
 
+import {RelatedContext} from 'components/related-context/related-context';
+
 import * as styles from './next-entry-preview.scss';
 
 const NextEntryPreview = ({data}: {data: Sources; countdown: number}) => {
+  const {relatedManager} = useContext(RelatedContext);
+
   return (
     <div className={styles.entryPreview}>
       <div className={styles.upNextText}>Up Next</div>
@@ -15,7 +21,7 @@ const NextEntryPreview = ({data}: {data: Sources; countdown: number}) => {
           <EntryImage {...{src: data.poster, duration: data.duration, type: data.type, width: 129, height: 72}} />
         </div>
         <div className={styles.controls}>
-          <div className={`${styles.control} ${styles.play}`}>
+          <div className={`${styles.control} ${styles.play}`} onClick={() => relatedManager?.playNext()}>
             <div className={styles.playContent}>
               <div className={styles.playIcon}>
                 <Icon type={IconType.Play} />
@@ -24,7 +30,7 @@ const NextEntryPreview = ({data}: {data: Sources; countdown: number}) => {
             </div>
           </div>
           <div className={`${styles.control} ${styles.cancel}`}>
-            <div className={styles.cancelContent}>Cancel</div>
+            <div className={styles.cancelText}>Cancel</div>
           </div>
         </div>
       </div>
