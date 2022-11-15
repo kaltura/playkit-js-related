16c7ff1bd612847245095d67eb26f3fc10147f05
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2022-11-15 16:28:06 +0200

    add index to services

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+import {EntryService} from './entry-service';
+import {ImageService} from './image-service';
+import {RelatedLoader} from './related-loader';
+
+export {EntryService, ImageService, RelatedLoader};
