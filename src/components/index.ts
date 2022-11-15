4c3a0aef14dcb65390364b83bb5250766ec9020b
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2022-11-15 16:27:53 +0200

    add index to components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import {ToggleButton} from './toggle-button/toggle-button';
+import {RelatedOverlay} from './related-overlay/related-overlay';
+import {Next} from './next/next';
+import {PrePlaybackPlayOverlayWrapper} from './pre-playback-play-overlay-wrapper/pre-playback-play-overlay-wrapper';
+
+export {Next, PrePlaybackPlayOverlayWrapper, RelatedOverlay, ToggleButton};
