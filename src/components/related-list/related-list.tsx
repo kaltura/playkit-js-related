5088552be57b9a1d642372daddb2ac65e55d9ca2
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2022-12-11 17:24:35 +0200

    scrollbar - add scrollbar to list

--- src/components/related-list/related-list.tsx
+++ src/components/related-list/related-list.tsx
@@ -1,3 +1,5 @@
+import {useRef, useState} from 'preact/hooks';
+
 const {withText} = KalturaPlayer.ui.preacti18n;
 const {PLAYER_SIZE} = KalturaPlayer.ui.components;
 
@@ -9,9 +11,16 @@ import * as styles from './related-list.scss';
 import {getListEntry} from 'components/related-grid/grid-utils';
 import {ImageService} from 'services';
 
+const SCROLL_BAR_TIMEOUT = 250;
+
 const RelatedList = withText({
   relatedVideosText: 'related.relatedVideos'
 })(({relatedManager, imageService, relatedVideosText}: {relatedManager: RelatedManager; imageService: ImageService; relatedVideosText: string}) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const [scrolling, setScrolling] = useState(false);
+  const [scrollTimeoutId, setScrollTimeoutId] = useState(-1);
+
   const data = relatedManager.entries;
   const entries = [];
 
@@ -19,6 +28,24 @@ const RelatedList = withText({
     const entryData = data[i];
     entries.push(entryData ? getListEntry(PLAYER_SIZE.MEDIUM, entryData) : <></>);
   }
+
+  const handleScroll = () => {
+    clearTimeout(scrollTimeoutId);
+    setScrolling(true);
+    setScrollTimeoutId(
+      window.setTimeout(() => {
+        setScrolling(false);
+      }, SCROLL_BAR_TIMEOUT)
+    );
+  };
+
+  const handleWheel = (e: WheelEvent) => {
+    e.preventDefault();
+    if (contentRef?.current) {
+      contentRef.current.scrollTop += e.deltaY;
+      handleScroll();
+    }
+  };
 
   return (
     <div className={styles.relatedList}>
@@ -31,7 +58,11 @@ const RelatedList = withText({
             }}
           />
         </div>
-        <div className={styles.content}>{entries}</div>
+        {/* <Scrollbar> */}
+        <div className={`${styles.content} ${scrolling ? styles.scrolling : ''}`} ref={contentRef} onScroll={handleScroll} onWheel={handleWheel}>
+          {entries}
+        </div>
+        {/* </Scrollbar> */}
       </RelatedContext.Provider>
     </div>
   );
