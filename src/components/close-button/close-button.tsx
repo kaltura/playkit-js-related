613ea8032a1328a77dcc708f902e3a7436b88da0
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2022-12-19 17:33:59 +0200

    add tooltip for close button

--- src/components/close-button/close-button.tsx
+++ src/components/close-button/close-button.tsx
@@ -1,20 +1,24 @@
 const {withText} = KalturaPlayer.ui.preacti18n;
-const {Icon, IconState} = KalturaPlayer.ui.components;
+const {Icon, IconState, Tooltip} = KalturaPlayer.ui.components;
 import {Icon as IconPath} from 'types';
 
 const CloseButton = withText({
   closeText: 'overlay.close'
 })(({onClick, closeText}: {closeText: string; onClick: () => void}) => {
   return (
-    <button aria-label={closeText} tabIndex={0} className={`${KalturaPlayer.ui.style.controlButton}`} onClick={onClick}>
-      <Icon
-        activeColor={KalturaPlayer.ui.style.white}
-        id={`related-close-icon`}
-        path={IconPath.CLOSE}
-        state={IconState.ACTIVE}
-        viewBox={`0 0 32 32`}
-      />
-    </button>
+    <div>
+      <Tooltip label={closeText}>
+        <button aria-label={closeText} tabIndex={0} className={`${KalturaPlayer.ui.style.controlButton}`} onClick={onClick}>
+          <Icon
+            activeColor={KalturaPlayer.ui.style.white}
+            id={`related-close-icon`}
+            path={IconPath.CLOSE}
+            state={IconState.ACTIVE}
+            viewBox={`0 0 32 32`}
+          />
+        </button>
+      </Tooltip>
+    </div>
   );
 });
 
