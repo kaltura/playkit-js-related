23598ce3b34872b66dbfeaa34453a439a3336d2f
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2023-01-05 13:08:58 +0200

    export RelatedEvent

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import {RelatedEvent} from 'types';
 import {Related} from './related';
 
 declare let __VERSION__: string;
@@ -8,6 +9,7 @@ const NAME = __NAME__;
 
 export {Related as Plugin};
 export {VERSION, NAME};
+export {RelatedEvent};
 
 const pluginName = 'related';
 
