139e980f9a0a3ad0cdb840b4f32246d2ec8eea0a
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2023-01-11 12:19:39 +0200

    add line-clamp to multiline-text

--- src/components/multiline-text/multiline-text.tsx
+++ src/components/multiline-text/multiline-text.tsx
@@ -52,7 +52,7 @@ const MultilineText = ({text, lineHeight, lines}: MultilineTextProps) => {
   }, [isTextFinalized, cutoffHeight, minLength, text, maxLength, currentLength]);
 
   return (
-    <div className={styles.multilineText}>
+    <div style={{'-webkit-line-clamp': lines}} className={styles.multilineText}>
       <div ref={ref}>{isTextFinalized ? finalizedText : text.slice(0, currentLength)}</div>
     </div>
   );
