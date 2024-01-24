6fa24aa2fb8134e6c456e4c794ad3b0e7eaa9ad8
JonathanTGold <62672270+JonathanTGold@users.noreply.github.com>
2024-01-24 09:15:10 +0200

    fix(FEC-13517): Wrong and inconsistent order of plugins (#61)
    fix: Wrong and inconsistent order of plugins
    
    fix: Plugins buttons sometimes display with the wrong icon
    
    solves: FEC-13517 FEC-13517
    
    Related Prs
    kaltura/playkit-js-ui-managers#51
    kaltura/playkit-js-transcript#175
    kaltura/playkit-js-share#38
    kaltura/playkit-js-moderation#74
    kaltura/playkit-js-playlist#53
    kaltura/playkit-js-navigation#348
    kaltura/playkit-js-info#90
    kaltura/playkit-js-navigation#348
    kaltura/playkit-js-downloads#39
    kaltura/playkit-js-qna#334
    
    ---------
    
    Co-authored-by: JonathanTGold <jonathan.gold@86@gmail.com>

--- src/related.tsx
+++ src/related.tsx
@@ -8,6 +8,8 @@ import {UpperBarManager, SidePanelsManager} from '@playkit-js/ui-managers';
 import {Icon, RelatedConfig} from 'types';
 import {RelatedInternalEvent} from 'event';
 
+export const pluginName = 'related';
+
 const PRESETS = ['Playback', 'Live'];
 
 /**
@@ -178,9 +180,14 @@ class Related extends KalturaPlayer.core.BasePlugin {
   addRelatedListComponents() {
     const {relatedManager} = this;
     if (this.iconId > 0 || !relatedManager.entries.length) return;
-
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     this.iconId = this.upperBarManager.add({
-      label: 'Related',
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      displayName: 'Related',
+      ariaLabel: 'Related',
+      order: 60,
       svgIcon: {
         viewBox: '0 0 32 32',
         path: Icon.LIST_TOGGLE
